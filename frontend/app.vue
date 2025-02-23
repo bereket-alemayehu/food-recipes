d8af99df5cbb494814fd0d2480b05944af4b34d1
Bereket <bereketalemayehuf@gmail.com>
2025-02-23 08:21:51 -0600

    Food Recipes

---
+++ frontend/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <!-- <NuxtRouteAnnouncer />
+    <NuxtWelcome /> -->
+    <div class="p-4 bg-gray-100 rounded-md mt-4">
+      <h2 class="text-2xl font-semibold mb-2">Welcome to Your Recipe App!</h2>
+      <p>Hello Bekisha</p>
+    </div>
+  </div>
+</template>
